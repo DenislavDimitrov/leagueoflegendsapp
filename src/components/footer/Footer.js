83308a06008754517d13e4d0b85cf9957e9765c3
DenislavDimitrov <70429056+DenislavDimitrov@users.noreply.github.com>
2021-04-03 20:34:11 +0300

    footer + some media

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={styles.nav}>
+            <ul>
+           Copyrights reserved by Riot Games 2021
+            </ul>
+        </footer>
+    )
+}
+export default Footer
