fd5f96c89dc1053ec5028174b761f559ffe5a897
DenislavDimitrov <70429056+DenislavDimitrov@users.noreply.github.com>
2021-04-03 20:58:35 +0300

    links

---
+++ src/components/linkComponent/LinkComponent.js
@@ -0,0 +1,15 @@
+import styles from "./Link.module.css"
+import {Link} from 'react-router-dom'
+
+const LinkComponent = ({ title, href }) => {
+    //to add links file
+    return (
+        <li className={styles.listItem}>
+            <Link to={href} className={styles["header-link"]} >
+                {title}
+            </Link>
+        </li>
+    )
+}
+
+export default LinkComponent
