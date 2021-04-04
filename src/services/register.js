ea6d90acfdfd3380ac103ec50baa135beee1dd26
DenislavDimitrov <70429056+DenislavDimitrov@users.noreply.github.com>
2021-04-04 17:44:46 +0300

    register service

---
+++ src/services/register.js
@@ -0,0 +1,24 @@
+const register = async (username, password) => {
+   
+    try {
+     const promise = await fetch('http://localhost:3003/api/user/register', {
+       method: 'POST',
+       body: JSON.stringify({
+           username,
+           password
+       }),
+       headers: {
+         'Content-Type': 'application/json'
+       }
+     })
+     const authToken = promise.headers.get('Authorization')
+     document.cookie = `x-auth-token=${authToken}`
+ 
+ 
+    } catch (e) {
+      //todo something
+    }
+     
+     
+ };
+ export default register;
